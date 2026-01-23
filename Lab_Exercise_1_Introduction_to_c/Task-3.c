0ebd4882a63360a24b1c867acc684d5f7f759dff
Ismailhosen342 <ismail234785@gmail.com>
2026-01-24 00:53:14 +0800

    add task3

---
+++ Lab_Exercise_1_Introduction_to_c/Task-3.c
@@ -0,0 +1,21 @@
+// Part C – Identifying Syntax Errors
+// 3. Task 3: Find and Fix the Errors
+// The following program contains syntax errors. Type it into your IDE, compile it, and correct
+// the errors.
+// #include <stdio.h>
+// int main()
+// {
+//  printf("Welcome to C Programming Lab")
+//  printf("This is your first program!);
+//  print("Let's learn together.\n")
+//  return 0
+// }
+
+#include<stdio.h>
+int main()
+{
+ printf("Welcome to C Programming Lab");
+ printf("This is your first program!");
+ printf("Let's learn together.\n");
+ return 0;
+}
