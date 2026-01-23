47861e3e0835710239a4e0f02ae6f495445b0e91
Ismailhosen342 <ismail234785@gmail.com>
2026-01-24 01:38:43 +0800

    add tatsk-1

--- Lab_Exercise_2_Basic_C_and_IO/Task-1.c
+++ Lab_Exercise_2_Basic_C_and_IO/Task-1.c
@@ -9,14 +9,13 @@ int main()
   float b = 2.647;
   char c = 'A';
   double d = 38.93940;
- float const PI = 3.1416;
+  float const PI = 3.1416;
 
-  
-  printf("Int value is:%.0d\n",a);
-  printf("Float value is:%.4f\n",b);
-  printf("Char value is:%c\n",c);
-  printf("Double value is:%.5lf\n",d);
-  printf("Const value is:%.2f\n",a);
-   
+  printf("Int value is:%.0d\n", a);
+  printf("Float value is:%.4f\n", b);
+  printf("Char value is:%c\n", c);
+  printf("Double value is:%.5lf\n", d);
+  printf("Const value is:%.2f\n", a);
+
   return 0;
 }
