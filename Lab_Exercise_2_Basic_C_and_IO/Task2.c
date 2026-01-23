20b2ec570313e30be0b3947cef69f52cb396e4c3
Ismailhosen342 <ismail234785@gmail.com>
2026-01-24 01:53:32 +0800

    add task-2

---
+++ Lab_Exercise_2_Basic_C_and_IO/Task2.c
@@ -0,0 +1,13 @@
+// TASK 2 : Understanding Data Types and Type Conversion(30 minutes)
+#include <stdio.h>
+int main()
+{
+ int a=5,b=2;
+ float result,cresult;
+ result=a/b;
+ cresult= (float)a/b;
+ printf("Integer division result:%f\n",result);
+ printf("Floating-point division result:%f", cresult);
+
+ return 0;
+}
