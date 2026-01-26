1fb9325a1404cfb6b2fee78f3a9c47d6e15cc913
Ismailhosen342 <ismail234785@gmail.com>
2026-01-27 00:21:04 +0800

    Add question

--- Lab_Exercise_4_Switch_Case/Task-1.c
+++ Lab_Exercise_4_Switch_Case/Task-1.c
@@ -1,2 +1,4 @@
-//Hello
-//new comment
+// Write a C program that reads an integer between 1 and 7 and prints the corresponding day
+// of the week.Use a switch..case statement.Example Output:
+// Enter day number(1 - 7) : 3 Wednesday
+
