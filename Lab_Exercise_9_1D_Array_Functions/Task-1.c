888ef66708511e4a267f804f6623f92f2ba1d675
Ismailhosen342 <ismailas564@gmail.com>
2026-01-30 23:39:52 +0800

    add Task-1

--- Lab_Exercise_9_1D_Array_Functions/Task-1.c
+++ Lab_Exercise_9_1D_Array_Functions/Task-1.c
@@ -8,3 +8,61 @@
 // e) Store all even numbers into a separate array called even[].
 // f) Store all odd numbers into another array called odd[].
 // g) Display both the even[] and odd[] arrays.
+
+#include <stdio.h>
+int arr[12], even[12], odd[12], ecount = 0, ocount = 0;
+void array()
+{
+  printf("Enter any 12 Numbers:");
+
+  for (int i = 1; i <= 12; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  printf("You entered number:");
+  for (int j = 0; j < 12; j++)
+  {
+    printf("%d ", arr[j]);
+  }
+  return;
+}
+
+void arrayoddeven()
+{
+  for (int i = 0; i < 12; i++)
+  {
+    if (arr[i] % 2 == 0)
+    {
+      even[ecount] = arr[i];
+      ecount++;
+    }
+    else
+    {
+      odd[ocount] = arr[i];
+      ocount++;
+    }
+  }
+  printf("\nThe odds Number:%d\n", ocount);
+  printf("The even Number:%d\n", ecount);
+
+  printf("The odd number is:");
+  for (int i = 0; i < ocount; i++)
+  {
+    printf("%d ", odd[i]);
+  }
+  printf("\nThe even number is:");
+  for (int i = 0; i < ocount; i++)
+  {
+    printf("%d ", even[i]);
+  }
+}
+
+int main()
+{
+
+  array();
+  arrayoddeven();
+
+  return 0;
+}
+//no copy past
