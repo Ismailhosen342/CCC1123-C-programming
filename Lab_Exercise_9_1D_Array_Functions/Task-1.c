c227c463977222960c89b781f24c3c1b0eef4223
Ismailhosen342 <ismailas564@gmail.com>
2026-01-30 19:01:46 +0800

    add task-1 question

--- Lab_Exercise_9_1D_Array_Functions/Task-1.c
+++ Lab_Exercise_9_1D_Array_Functions/Task-1.c
@@ -1,1 +1,10 @@
-//Task1   cmmitcheck agai
+// Exercise 1: Array Manipulation – Even and Odd Numbers
+// Write a C program that performs operations on a one-dimensional array of integers.
+// Requirements:
+// a) Create a function arrayOddEven()
+// b) Declare a 1D integer array of size 12.
+// c) Allow the user to input 12 integers into the array.
+// d) Count and display the number of even and odd numbers in the array.
+// e) Store all even numbers into a separate array called even[].
+// f) Store all odd numbers into another array called odd[].
+// g) Display both the even[] and odd[] arrays.
